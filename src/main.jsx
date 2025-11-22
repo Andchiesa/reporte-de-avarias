7025fc1a8f22beb63b48fdd29c07283d67294399
Anderson Chiesa <80640593+Andchiesa@users.noreply.github.com>
2025-11-22 14:41:31 -0300

    Remove index.css import from main.jsx
    Removed the import statement for index.css.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx' // Certifique-se que renomeou sua apresentação para App.jsx
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
